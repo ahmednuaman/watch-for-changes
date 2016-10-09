980e3945eb9189d9a2cb3ebba78dc8e18ae96d2e
Ahmed Nuaman <ahmed@ahmednuaman.com>
2016-10-09 19:08:13 +0100

    set up parsing

--- parse.js
+++ parse.js
@@ -4,33 +4,44 @@ import { diffWords } from 'diff'
 import awsConfig from './aws.config.json'
 import browser from './lib/browser'
 import db from './lib/db'
-import tables from './table.config.json'
+import queue from './lib/queue'
+import queueConfig from './queue.config.json'
+import tableConfig from './table.config.json'
 
 config.update(awsConfig)
 
 const handler = ({ url, html }, context, callback) => {
-  const targets = await db.get({ url }, tables.targets.name)
+  const targets = await db.get({ url }, tableConfig.targets.name)
   const window = await browser(html)
 
   const newTargets = _.transform(targets, (result, target) => {
     result[target] = window.jQuery(target).text()
   }, {})
 
-  db.put({
-    url
-    targets: newTargets
-  }, tables.targets.name)
-
   if (!_.eq(targets, newTargets)) {
+    const diffs = _.transform(targets, (result, text, target) => {
+      result[target] = diffWords(text, newTargets[target])
+    }, {})
+
     await db.put({
       url,
-      Date.now(): _.transform(targets, (result, text, target) => {
-        result[target] = diffWords(text, newTargets[target])
-      }, {})
+      targets: newTargets
+    }, tableConfig.targets.name)
+
+    await db.put({
+      url,
+      Date.now(): diffs
     })
 
-    
+    await queue.send(queueConfig.diffs, url, {
+      diffs: {
+        DataType: 'string',
+        StringValue: JSON.stringify(diffs)
+      }
+    })
   }
+
+  callback()
 }
 
 export { handler }
